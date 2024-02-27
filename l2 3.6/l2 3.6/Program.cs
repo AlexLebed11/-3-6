3670af916c2cbcdd8307e0cbdf9ec3dc55932176
AlexLebed11 <121554480+AlexLebed11@users.noreply.github.com>
2024-02-27 12:48:59 +0200

    lab2

---
+++ l2 3.6/l2 3.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+namespace l1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(new ParameterizedThreadStart(Count!));
+            myThread.Start(); // передаємо змінну у новий потік 
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Потiк 1 \tTime: " + DateTime.Now);
+                Thread.Sleep(300);
+            }
+            Console.ReadLine();
+        }
+        public static void Count(object x)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("\tПотiк 2 \t Time:" + DateTime.Now);
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
