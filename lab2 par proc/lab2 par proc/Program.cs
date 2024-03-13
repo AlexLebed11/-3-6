f29f08e8ba3a81ca27cd2f7234c9e092ac7641c3
AlexLebed11 <121554480+AlexLebed11@users.noreply.github.com>
2024-03-13 12:10:01 +0200

    Лабораторная 2 пункт 2

---
+++ lab2 par proc/lab2 par proc/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Channels;
+
+class Program 
+{
+
+    static void Main(string[] args)
+    {
+        int num = 0;
+
+        num = Int32.Parse(Console.ReadLine()!);
+
+        Process newProc = new Process();
+        newProc.StartInfo.FileName =
+       @"C:\Users\root\source\repos\l_2_2\l_2_2\bin\Debug\net6.0\l_2_2.exe";
+        
+        newProc.StartInfo.UseShellExecute = true;
+
+        while (num != 0)
+        {
+            newProc.Start();
+            Console.WriteLine("Новий процес створений.\n");
+            newProc.Close();
+            Console.WriteLine("Новий процес закiнчений.\n");
+
+            num--;
+        }
+    }
+
+}
