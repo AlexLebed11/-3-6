0b84202d29f63732852973a951c89ded82374476
AlexLebed11 <121554480+AlexLebed11@users.noreply.github.com>
2024-03-14 11:52:35 +0200

    Лабораторная 3 пункт 2

---
+++ l_3 p2/l_3 p2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Channels;
+using System.Collections.Generic;
+
+namespace l_3_2_1_p
+{
+    class Program
+    {
+
+        static object lockOn = new object(); 
+        static public void Run()
+        {
+            //lock (lockOn)
+            //{
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                Console.WriteLine("Дочiрнiй процес.");
+                for (int j = 10; j <= 12; j++)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.Write(j + " ");
+                        Thread.Sleep(100);
+                    }
+                    Console.WriteLine();
+                }
+            //}
+        }
+
+        static void Main(string[] args)
+        {
+            List<Thread> threads = new List<Thread>();
+            int num = 0;
+
+            num = Int32.Parse(Console.ReadLine()!);
+
+
+            for (int i = 0; i < num; i++)
+            {
+                Thread thread = new Thread(new ThreadStart(() => Run())) ;
+
+                threads.Add(thread);
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+        }
+    }
+}
